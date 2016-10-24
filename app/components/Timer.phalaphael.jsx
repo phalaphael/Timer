f90b87d709181c2b9104bdda7e3fbc9dcc2d1f42
Piotr Roczniak <proczniak@gmail.com>
2016-10-24 12:06:52 +0200

    created timer component and tests

---
+++ app/components/Timer.phalaphael.jsx
@@ -0,0 +1,69 @@
+var React = require('react');
+var Clock = require('Clock');
+var Controls = require('Controls');
+
+var Timer = React.createClass({
+
+  getInitialState: function(){
+    return {
+      count: 0,
+      countdownStatus: 'paused'
+    };
+  },
+  componentDidUpdate: function (prevProps, prevState) {
+    if (this.state.countdownStatus !== prevState.countdownStatus){
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+        case 'stopped':
+          this.setState({count:0, countdownStatus: 'paused'});
+        case 'paused':
+          clearInterval(this.timer);
+          this.timer = undefined;
+          break;
+      }
+    }
+  },
+  componentWillUnmount: function() {
+    clearInterval(this.timer);
+    this.timer=undefined;
+  },
+  startTimer: function(){
+    this.timer = setInterval(() => {
+      var newCount = this.state.count +1;
+      this.setState({
+        count: newCount //>= 0 ? newCount: 0
+      });
+
+      if (newCount === 0) {
+        this.setState({countdownStatus:'stopped'});
+      }
+
+    }, 1000);
+  },
+  handleSetCountdown: function(seconds){
+    this.setState({
+      count: seconds,
+      countdownStatus: 'started'
+    });
+  },
+  handleStatusChange: function (newStatus){
+    this.setState({countdownStatus:newStatus});
+  },
+  render: function(){
+    var {count, countdownStatus} = this.state;
+    var renderControlArea = () => {
+      return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>
+    };
+    return (
+      <div>
+        <h1 className="page-title">Timer App</h1>
+        <Clock totalSeconds={count}/>
+          {renderControlArea()}
+    </div>
+    );
+  }
+});
+
+module.exports = Timer;
